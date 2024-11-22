aaaad9b8c20035e9598841af74ec4106e43b5919
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-11-22 13:10:35 -0500

    added flask freezer code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, Blueprint
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -10,9 +10,13 @@ def home():
 def serve_pkg(filename):
     return send_from_directory('pkg', filename)
 
-@app.route('/fermat')
-def serve_fermat():
-    return render_template("fermat.html")
+# @app.route('/fermat')
+# def serve_fermat():
+#     return render_template("fermat.html")
+
+@app.route('/template_<path:filename>')
+def serve_template(filename):
+    return render_template(filename)
 # @app.route('/static/<path:filename>')
 # def serve_static(filename):
 #     return send_from_directory('static', filename)
