4fb7e30330fc0d37269f46f8f9db20cf649fdbd4
enabledornot <102371948+enabledornot@users.noreply.github.com>
2024-12-05 13:49:16 -0500

    added header and descrption to web pages

--- static/aks.js
+++ static/aks.js
@@ -11,9 +11,9 @@ async function run() {
         //     aes.push(parseInt(value));
         // });
         const result = aks_test(p_prime);
-        const c = result;
-        // const c = result.result;
-        // const pw = result.pow_result;
+        const c = result.result;
+        const r = result.r;
+        const step = result.step;
         // $('#fermat_eq').empty();
         // for (var i = 0; i < pw.length; i++) {
         //     $('#fermat_eq').append(`<p> \\[${aes[i]}^{${p_prime-1}} \\mod ${p_prime} \\equiv ${pw[i]}\\] </p>`);
